5a34fd268f1e148ecde67cd92c2131befb420aaf
XIN <61730607@qq.com>
2021-11-14 17:45:37 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -77,7 +77,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 # html_theme = 'sphinx_book_theme'
 # html_theme = 'pytorch_sphinx_theme'
-html_theme_path = [html_theme.get_html_theme_path()]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # html_theme_options = {
 #     'menu': [
